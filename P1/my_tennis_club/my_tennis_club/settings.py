936f1cea4a811cdfaf5099378a278c58771f4948
LokiAngelMx <LokiGarciaMx@gmail.com>
2023-10-17 08:30:18 -0600

    Práctica 9

--- P1/my_tennis_club/my_tennis_club/settings.py
+++ P1/my_tennis_club/my_tennis_club/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "my_tennis_club.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'Loki',
+        'PASSWORD': 'D06defebrerodel203',
+        'HOST': 'lokidatabase.ctenakabgjl1.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
