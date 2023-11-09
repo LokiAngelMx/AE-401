7f5dbc893ed84d86de9146aced5a332b11a69ad7
LokiAngelMx <LokiGarciaMx@gmail.com>
2023-11-09 09:35:21 -0600

    Blog actualizado

--- BlogApplication/mysite/blog/models.py
+++ BlogApplication/mysite/blog/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+            .filter(status = Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -17,6 +22,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now = True)
     update = models.DateTimeField(auto_now = True)
     status = models.CharField(max_length = 2, choices = Status.choices, default = Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish',]
