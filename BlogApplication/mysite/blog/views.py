da3aefb9487dc763bc6011aa8d625a6bc340ea08
LokiAngelMx <LokiGarciaMx@gmail.com>
2023-11-21 08:43:24 -0600

    Agregar CSS

--- BlogApplication/mysite/blog/views.py
+++ BlogApplication/mysite/blog/views.py
@@ -3,15 +3,16 @@ from .models import Post
 from django.http import Http404
 
 # Create your views here.
-
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 
+                  'blog/post/list.html', 
+                  {'posts': posts})
 
-def post_detail(request, id):
+def post_detail (request, id):
     post = get_object_or_404(Post, id = id, status = Post.Status.PUBLISHED)
     try:
-        post = Post.published.get(id = id)
+        post = Post.published.get(id=id)
     except Post.DoesNotExist:
-        raise Http404("Post does not exist")
-    return render(request, 'blog/post/detail.html', {'post': post})
+        raise Http404 ("No Post found.")
+    return render (request, 'blog/post/detail.html', {'post': post})
